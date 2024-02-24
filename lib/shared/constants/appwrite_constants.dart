21a8aa1939a43e3f90e0e80a61f7eb63c7ffa19a
levantan1401 <levantan1401.developer@gmail.com>
2024-02-24 22:37:31 +0700

    Constant appwrite

--- lib/shared/constants/appwrite_constants.dart
+++ lib/shared/constants/appwrite_constants.dart
@@ -2,11 +2,13 @@ class AppWriteConstants {
   static String endPoint = "https://cloud.appwrite.io/v1";
   static String endPoint2 = "https://reset-password-alpha.vercel.app/v1";
 
-  static String projectId = "";
-  static String databaseId = "";
-  static String usersCollection = "";
-  static String binsCollection = '';
+  static String projectId = "65d97bc4695c6bcbc83d";
+  static String databaseId = "65d9eeb4be97fba1f8b6";
+  static String userRequestTrashCollection = "65d9eedea7d5c425131b";
+  static String usersCollection = "65d9eeca5c330a39022c";
 
+
+  static String binsCollection = '';
   static String database_bin_Id = "";
   static String collection_bin_Id = "";
 
@@ -14,4 +16,6 @@ class AppWriteConstants {
   static String categoryBucketId = "";
   static String trashBucketId = "";
   static String categoryDetailCollectionId = "";
+
+  
 }
