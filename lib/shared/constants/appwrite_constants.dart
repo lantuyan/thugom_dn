c9f970ff6879ab1790e5a3dd4b14eb03ee3bc2c1
vnghia20it11 <vptnghia.20it11@vku.udn.vn>
2024-02-25 23:41:00 +0700

    feature/map_user

--- lib/shared/constants/appwrite_constants.dart
+++ lib/shared/constants/appwrite_constants.dart
@@ -3,15 +3,14 @@ class AppWriteConstants {
   static String endPoint2 = "https://reset-password-alpha.vercel.app/v1";
 
   static String projectId = "65d97bc4695c6bcbc83d";
-  static String databaseId = "65788ebd84b8c4cf9047";
-  static String usersCollection = "65d4e86a46ee2a20c955";
+  static String databaseId = "65d9eeb4be97fba1f8b6";
+  static String usersCollection = "";
   static String binsCollection = '';
 
-  static String database_bin_Id = "65788ebd84b8c4cf9047";
-  static String collection_bin_Id = "65d4e85ff0a8ca113f3c";
+  static String collection_point_Id = "65db36de750ed6145af1";
 
-  static String categoryCollectionId = "656c7e48402288c32e78";
-  static String categoryBucketId = "65af75a8d36cab411474";
-  static String trashBucketId = "65af761d13c380049957";
-  static String categoryDetailCollectionId = "656cfea2bd044a13e679";
+  static String categoryCollectionId = "";
+  static String categoryBucketId = "";
+  static String trashBucketId = "";
+  static String categoryDetailCollectionId = "";
 }
