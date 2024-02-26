02f3310e03f10ff8d541810edf78c9efabfdb833
Van Chuong <134tutu431@gmail.com>
2024-02-26 22:13:17 +0700

    Update custom_button_style.dart

--- lib/shared/themes/style/custom_button_style.dart
+++ lib/shared/themes/style/custom_button_style.dart
@@ -14,7 +14,7 @@ class CustomButtonStyle {
     );
 
     static ButtonStyle transparentButton = ElevatedButton.styleFrom(
-        backgroundColor: ColorsConstants.kBackgroundColor,
+        backgroundColor: ColorsConstants.ksecondBackgroundColor,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
           side: const BorderSide(
