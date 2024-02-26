dd4c2534f0f651cb86fc0863dfcd08865a994dd5
lantuyan <76894132+lantuyan@users.noreply.github.com>
2024-02-26 20:39:34 +0700

    Update appwrite_constants.dart

--- lib/shared/constants/appwrite_constants.dart
+++ lib/shared/constants/appwrite_constants.dart
@@ -4,18 +4,10 @@ class AppWriteConstants {
 
   static String projectId = "65d97bc4695c6bcbc83d";
   static String databaseId = "65d9eeb4be97fba1f8b6";
-  static String userRequestTrashCollection = "65d9eedea7d5c425131b";
+ 
   static String usersCollection = "65d9eeca5c330a39022c";
-
-
-  static String binsCollection = '';
-  static String database_bin_Id = "";
-  static String collection_bin_Id = "";
-
-  static String categoryCollectionId = "";
-  static String categoryBucketId = "";
-  static String trashBucketId = "";
-  static String categoryDetailCollectionId = "";
-
+  
+  static String collection_point_Id = "65db36de750ed6145af1";
+  static String userRequestTrashCollection = "65d9eedea7d5c425131b";
   
 }
