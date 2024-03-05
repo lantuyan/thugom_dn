c489510cee6739bac43d494b04585752e63bbd61
Van Chuong <134tutu431@gmail.com>
2024-03-06 02:15:41 +0700

    create base flow

---
+++ lib/views/main/home/statistic/line_chart_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LineChartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Line Chart'),
+      ),
+    );
+  }
+
+}
